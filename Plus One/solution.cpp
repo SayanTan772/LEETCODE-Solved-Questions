1cd74901c23d3fd64bb114053c5f5a6ef6935d7c
SayanTan772 <sayantandey506@gmail.com>
2025-01-29 23:17:47 +0530

    added new solution

---
+++ Plus One/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+        
+        std::vector<int> Number(n + 1, 0);
+        Number[0] = 1;
+        
+        return Number;
+    }
+};
